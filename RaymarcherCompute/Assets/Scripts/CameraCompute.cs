a3066e1da9471dc7a342e5541180b46aadef29e8
Riley Mahr <rwmahr@gmail.com>
2019-11-18 14:07:14 -0600

    adding initial Unity project

---
+++ RaymarcherCompute/Assets/Scripts/CameraCompute.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class CameraCompute : MonoBehaviour
+{
+
+    public ComputeShader fractalShader;
+
+    // Initialize any variables here
+
+    // Public variables are editable in Unity from the UI and other code files
+    public int publicInteger = 0;
+
+    // Serialized private variables are editable in the UI but not from other code files
+    [SerializeField]
+    private int serializedPrivateInteger = 0;
+
+    // Private, non-serialized variables cannot be edited in the UI or from other code files
+    private int privateInteger = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

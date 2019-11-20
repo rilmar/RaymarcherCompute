d7f3a4fc0e301bc5f0400b560a10e73abee4631d
Riley Mahr <rwmahr@gmail.com>
2019-11-19 18:16:43 -0600

    adding readme

--- RaymarcherCompute/Assets/Scripts/CameraCompute.cs
+++ RaymarcherCompute/Assets/Scripts/CameraCompute.cs
@@ -2,34 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Credit to http://blog.three-eyed-games.com/2018/05/03/gpu-ray-tracing-in-unity-part-1/
+ * for initial setup of shader code in Unity
+ */
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class CameraCompute : MonoBehaviour
 {
+    public ComputeShader RayTracingShader;
+    private RenderTexture _target;
+    private Camera _camera;
 
-    public ComputeShader fractalShader;
-
-    // Initialize any variables here
-
-    // Public variables are editable in Unity from the UI and other code files
-    public int publicInteger = 0;
-
-    // Serialized private variables are editable in the UI but not from other code files
-    [SerializeField]
-    private int serializedPrivateInteger = 0;
-
-    // Private, non-serialized variables cannot be edited in the UI or from other code files
-    private int privateInteger = 0;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        _camera = GetComponent<Camera>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetShaderParameters()
     {
-        
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
     }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Render(destination);
+    }
+
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a current render target
+        InitRenderTexture();
+        // Set the target and dispatch the compute shader
+        RayTracingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        // Blit the result texture to the screen
+        Graphics.Blit(_target, destination);
+    }
+
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null)
+                _target.Release();
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+
 }

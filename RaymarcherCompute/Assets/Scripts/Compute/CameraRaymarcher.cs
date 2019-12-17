0072640b974910af03532a33ec0872829d69418c
Riley Mahr <rwmahr@gmail.com>
2019-12-17 12:15:00 -0600

    RayMarching
    adding raymarching

--- RaymarcherCompute/Assets/Scripts/Compute/CameraRaymarcher.cs
+++ RaymarcherCompute/Assets/Scripts/Compute/CameraRaymarcher.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class CameraRaymarcher : CameraCompute
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Light sceneLight;
+
+
+    public override void GetSceneInformation()
     {
-        
+        base.GetSceneInformation();
+        sceneLight = FindObjectOfType<Light>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void SetShaderUserParameters(RenderTexture source)
     {
-        
+        base.SetShaderUserParameters(source);
+        // UserShader.SetTexture(0, "_ImageInput", source); // this is the surrounding unity scene
+
+        //UserShader.SetFloat("_Power", power);
+        //UserShader.SetFloat("_Epsilon", (epsilon / 5000.0f));
+        //UserShader.SetFloat("_MaxDistance", maxDistance);
+        //UserShader.SetFloat("_MaxSteps", maxSteps);
+        //UserShader.SetFloat("_Rim", rimLight);
+
+        //UserShader.SetVector("_LightDirection", sceneLight.transform.forward);
+
+    }
+
+    private void Update()
+    {
+    //    if (Input.GetKey(powerIncreaseKey))
+    //    {
+    //        power += powerStep;
+    //        power = Mathf.Clamp(power, powerMin, powerMax);
+
+
+    //    }
+    //    else if (Input.GetKey(powerDecreaseKey))
+    //    {
+    //        power -= powerStep;
+    //        power = Mathf.Clamp(power, powerMin, powerMax);
+    //    }
+
     }
 }

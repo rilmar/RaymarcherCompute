19fd2fc281c758715cd090909bbb5d69f846a49e
Riley Mahr <rwmahr@gmail.com>
2019-12-17 17:40:34 -0600

    Adding interactivity and scene selection

--- RaymarcherCompute/Assets/Scripts/Compute/CameraRaymarcher.cs
+++ RaymarcherCompute/Assets/Scripts/Compute/CameraRaymarcher.cs
@@ -7,6 +7,24 @@ public class CameraRaymarcher : CameraCompute
 
     private Light sceneLight;
 
+    [Range(0, 3)]
+    public int SceneMode;
+
+    public Transform objectPosition;
+
+    public float surfaceThreshold = 0.0005f;
+
+    [Range(0,1)]
+    public float colorModifier;
+
+    public float Scale;
+    public KeyCode ScaleUp;
+    public KeyCode ScaleDown;
+
+    public float MinScale = 0.5f;
+    public float MaxScale = 2.0f;
+    public float ScaleStep = 0.1f;
+
 
     public override void GetSceneInformation()
     {
@@ -19,11 +37,14 @@ public class CameraRaymarcher : CameraCompute
         base.SetShaderUserParameters(source);
         // UserShader.SetTexture(0, "_ImageInput", source); // this is the surrounding unity scene
 
-        //UserShader.SetFloat("_Power", power);
-        //UserShader.SetFloat("_Epsilon", (epsilon / 5000.0f));
-        //UserShader.SetFloat("_MaxDistance", maxDistance);
-        //UserShader.SetFloat("_MaxSteps", maxSteps);
-        //UserShader.SetFloat("_Rim", rimLight);
+        UserShader.SetInt("_SceneMode", SceneMode);
+        UserShader.SetFloat("_SurfaceThreshold", surfaceThreshold);
+
+        UserShader.SetFloat("_ColorModifier", colorModifier);
+        float[] f = { objectPosition.position.x, objectPosition.position.y, objectPosition.position.z };
+        UserShader.SetFloats("_Position", f);
+        float[] s = { Scale, Scale, Scale };
+        UserShader.SetFloats("_Scale", s);
 
         //UserShader.SetVector("_LightDirection", sceneLight.transform.forward);
 
@@ -31,18 +52,33 @@ public class CameraRaymarcher : CameraCompute
 
     private void Update()
     {
-    //    if (Input.GetKey(powerIncreaseKey))
-    //    {
-    //        power += powerStep;
-    //        power = Mathf.Clamp(power, powerMin, powerMax);
+        if (Input.GetKey(ScaleUp))
+        {
+            Scale += ScaleStep;
+            Scale = Mathf.Clamp(Scale, MinScale, MaxScale);
 
 
-    //    }
-    //    else if (Input.GetKey(powerDecreaseKey))
-    //    {
-    //        power -= powerStep;
-    //        power = Mathf.Clamp(power, powerMin, powerMax);
-    //    }
-
+        }
+        else if (Input.GetKey(ScaleDown))
+        {
+            Scale -= ScaleStep;
+            Scale = Mathf.Clamp(Scale, MinScale, MaxScale);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneMode = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneMode = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneMode = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneMode = 3;
+        }
     }
 }
---
+++ RaymarcherCompute/Assets/Scripts/Interaction/PlayerMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public float constraintX;
+    public float constraintY;
+
+    public float moveSpeed;
+
+    public KeyCode forward, back, left, right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(right))
+        {
+            Vector3 currentTransform = transform.position;
+            float currentX = transform.position.x;
+            currentX += moveSpeed;
+            currentX = Mathf.Clamp(currentX, -constraintX, constraintX);
+            transform.position = new Vector3(currentX, currentTransform.y, currentTransform.z);
+
+
+        }
+        else if (Input.GetKey(left))
+        {
+            Vector3 currentTransform = transform.position;
+            float currentX = transform.position.x;
+            currentX -= moveSpeed;
+            currentX = Mathf.Clamp(currentX, -constraintX, constraintX);
+            transform.position = new Vector3(currentX, currentTransform.y, currentTransform.z);
+        }
+    }
+}

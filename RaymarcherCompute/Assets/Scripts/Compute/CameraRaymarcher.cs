248e67747097ef6958503dbae81f49407f0a580d
Riley Mahr <rwmahr@gmail.com>
2019-12-17 19:59:50 -0600

    Adding control of cube
    I added a screenchot script, but due to overhead in creating a consistent file path,. it will be disabled for the time being

--- RaymarcherCompute/Assets/Scripts/Compute/CameraRaymarcher.cs
+++ RaymarcherCompute/Assets/Scripts/Compute/CameraRaymarcher.cs
@@ -10,16 +10,20 @@ public class CameraRaymarcher : CameraCompute
     [Range(0, 3)]
     public int SceneMode;
 
-    public Transform objectPosition;
+    public Transform objectAPosition;
+    public Transform objectBPosition;
 
     public float surfaceThreshold = 0.0005f;
 
     [Range(0,1)]
     public float colorModifier;
 
-    public float Scale;
-    public KeyCode ScaleUp;
-    public KeyCode ScaleDown;
+    public float AScale;
+    public float BScale;
+    public KeyCode AScaleUp;
+    public KeyCode AScaleDown;
+    public KeyCode BScaleUp;
+    public KeyCode BScaleDown;
 
     public float MinScale = 0.5f;
     public float MaxScale = 2.0f;
@@ -41,10 +45,18 @@ public class CameraRaymarcher : CameraCompute
         UserShader.SetFloat("_SurfaceThreshold", surfaceThreshold);
 
         UserShader.SetFloat("_ColorModifier", colorModifier);
-        float[] f = { objectPosition.position.x, objectPosition.position.y, objectPosition.position.z };
-        UserShader.SetFloats("_Position", f);
-        float[] s = { Scale, Scale, Scale };
-        UserShader.SetFloats("_Scale", s);
+
+        float[] fa = { objectAPosition.position.x, objectAPosition.position.y, objectAPosition.position.z };
+        UserShader.SetFloats("_APosition", fa);
+
+        float[] fb = { objectBPosition.position.x, objectBPosition.position.y, objectBPosition.position.z };
+        UserShader.SetFloats("_BPosition", fb);
+
+        float[] sa = { AScale, AScale, AScale };
+        UserShader.SetFloats("_AScale", sa);
+
+        float[] sb = { BScale, BScale, BScale };
+        UserShader.SetFloats("_BScale", sb);
 
         //UserShader.SetVector("_LightDirection", sceneLight.transform.forward);
 
@@ -52,18 +64,32 @@ public class CameraRaymarcher : CameraCompute
 
     private void Update()
     {
-        if (Input.GetKey(ScaleUp))
+        if (Input.GetKey(AScaleUp))
         {
-            Scale += ScaleStep;
-            Scale = Mathf.Clamp(Scale, MinScale, MaxScale);
+            AScale += ScaleStep;
+            AScale = Mathf.Clamp(AScale, MinScale, MaxScale);
 
 
         }
-        else if (Input.GetKey(ScaleDown))
+        else if (Input.GetKey(AScaleDown))
         {
-            Scale -= ScaleStep;
-            Scale = Mathf.Clamp(Scale, MinScale, MaxScale);
+            AScale -= ScaleStep;
+            AScale = Mathf.Clamp(AScale, MinScale, MaxScale);
         }
+
+        if (Input.GetKey(BScaleUp))
+        {
+            BScale += ScaleStep;
+            BScale = Mathf.Clamp(BScale, MinScale, MaxScale);
+
+
+        }
+        else if (Input.GetKey(BScaleDown))
+        {
+            BScale -= ScaleStep;
+            BScale = Mathf.Clamp(BScale, MinScale, MaxScale);
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             SceneMode = 0;
---
+++ RaymarcherCompute/Assets/Scripts/Interaction/Screenshot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    public KeyCode screenshotKey = KeyCode.Space;
+    private int index = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(screenshotKey))
+        {
+            for(int i = index; i < 1000; i++)
+            {
+                if (!System.IO.File.Exists("raymarch" + i + ".png"))
+                {
+                    ScreenCapture.CaptureScreenshot("raymarch" + i + ".png", 4);
+                    Debug.Log("Screenshot taken: raymarch" + i + ".png");
+                    index = i;
+                    break;
+                }
+            }
+        }
+    }
+}
